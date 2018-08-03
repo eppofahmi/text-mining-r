1ed216e19cf6b0e6b4295ae725ac0e8b78d6772b
eppofahmi <eppofahmi@gmail.com.com>
2018-08-04 04:38:43 +0700

    1st attemp

--- R/SA_1.R
+++ R/SA_1.R
@@ -1,8 +1,148 @@
-# Sentiment analysis 1 
-# use ML to do SA
+# Sentiment analysis using ML to do SA
+# source: https://datascienceplus.com/sentiment-analysis-with-machine-learning-in-r/
 
-library(tidyverse)
-library(tidytext)
-library(lubridate)
-library(textclean)
+library(RTextTools)
+library(e1071)
+
+# 1. data ----
+pos_tweets =  rbind(
+  c('I love this car', 'positive'),
+  c('This view is amazing', 'positive'),
+  c('I feel great this morning', 'positive'),
+  c('I am so excited about the concert', 'positive'),
+  c('He is my best friend', 'positive'))
+
+neg_tweets = rbind(
+  c('I do not like this car', 'negative'),
+  c('This view is horrible', 'negative'),
+  c('I feel tired this morning', 'negative'),
+  c('I am not looking forward to the concert', 'negative'),
+  c('He is my enemy', 'negative'))
+
+test_tweets = rbind(
+  c('feel happy this morning', 'positive'),
+  c('larry friend', 'positive'),
+  c('not like that man', 'negative'),
+  c('house not great', 'negative'),
+  c('your song annoying', 'negative'))
+
+tweets = rbind(pos_tweets, neg_tweets, test_tweets)
+
+# 2. build dtm ----
+matrix= create_matrix(tweets[,1], language="english", 
+                      removeStopwords=FALSE, removeNumbers=TRUE, 
+                      stemWords=FALSE)
+
+# Now, we can train the naive Bayes model with the training set. Note that, `e1071` asks the response variable to be numeric or factor. Thus, we convert characters to factors here. This is a little trick.
+
+# 3. train the model ----
+mat = as.matrix(matrix)
+classifier = naiveBayes(mat[1:10,], as.factor(tweets[1:10,2]) )
+
+# test the validity
+predicted = predict(classifier, mat[11:15,]); predicted 
+table(tweets[11:15, 2], predicted)
+recall_accuracy(tweets[11:15, 2], predicted)
+
+# 4. rtext model ----
+# build the data to specify response variable, training set, testing set.
+container = create_container(matrix, as.numeric(as.factor(tweets[,2])),
+                             trainSize=1:10, testSize=11:15, virgin=FALSE)
+
+# 5. train using multi ML ----
+models = train_models(container, algorithms=c("MAXENT", "SVM", "RF", "BAGGING", "TREE"))
+
+results = classify_models(container, models)
+
+# 6. accuracy -----
+# accuracy table
+table(as.numeric(as.factor(tweets[11:15, 2])), results[,"FORESTS_LABEL"])
+table(as.numeric(as.factor(tweets[11:15, 2])), results[,"MAXENTROPY_LABEL"])
+
+# recall accuracy
+recall_accuracy(as.numeric(as.factor(tweets[11:15, 2])), results[,"FORESTS_LABEL"])
+recall_accuracy(as.numeric(as.factor(tweets[11:15, 2])), results[,"MAXENTROPY_LABEL"])
+recall_accuracy(as.numeric(as.factor(tweets[11:15, 2])), results[,"TREE_LABEL"])
+recall_accuracy(as.numeric(as.factor(tweets[11:15, 2])), results[,"BAGGING_LABEL"])
+recall_accuracy(as.numeric(as.factor(tweets[11:15, 2])), results[,"SVM_LABEL"])
+
+# 7. model summary ----
+analytics = create_analytics(container, results)
+summary(analytics)
+head(analytics@document_summary)
+analytics@ensemble_summary
+
+# 8. cross validate ----
+N=4
+set.seed(2014)
+cross_validate(container, N, "MAXENT")
+cross_validate(container, N, "TREE")
+cross_validate(container, N, "SVM")
+cross_validate(container, N, "RF")
+
+# Sentiment analysis for tweets ----
+###################
+"load data"
+###################
+setwd("/Volumes/mydata/RStudio/sentiment_analysis/data")
+happy = readLines("./happy.txt")
+sad = readLines("./sad.txt")
+happy_test = readLines("./happy_test.txt")
+sad_test = readLines("./sad_test.txt")
+
+tweet = c(happy, sad)
+tweet_test= c(happy_test, sad_test)
+tweet_all = c(tweet, tweet_test)
+sentiment = c(rep("happy", length(happy) ), rep("sad", length(sad)))
+sentiment_test = c(rep("happy", length(happy_test) ), rep("sad", length(sad_test)))
+sentiment_all = as.factor(c(sentiment, sentiment_test))
+
+# try naive bayes ----
+mat= create_matrix(tweet_all, language="english", 
+                   removeStopwords=FALSE, removeNumbers=TRUE, 
+                   stemWords=FALSE, tm::weightTfIdf)
+
+mat = as.matrix(mat)
+
+classifier = naiveBayes(mat[1:160,], as.factor(sentiment_all[1:160]))
+predicted = predict(classifier, mat[161:180,]); predicted
+
+table(sentiment_test, predicted)
+recall_accuracy(sentiment_test, predicted)
+
+# other methods ----
+mat= create_matrix(tweet_all, language="english", 
+                   removeStopwords=FALSE, removeNumbers=TRUE, 
+                   stemWords=FALSE, tm::weightTfIdf)
+
+container = create_container(mat, as.numeric(sentiment_all),
+                             trainSize=1:160, testSize=161:180,virgin=FALSE)
+
+models = train_models(container, algorithms=c("MAXENT",
+                                              "SVM",
+                                              #"GLMNET", "BOOSTING", 
+                                              "SLDA","BAGGING", 
+                                              "RF", # "NNET", 
+                                              "TREE" 
+))
+
+# test the model
+results = classify_models(container, models)
+table(as.numeric(as.numeric(sentiment_all[161:180])), results[,"FORESTS_LABEL"])
+recall_accuracy(as.numeric(as.numeric(sentiment_all[161:180])), results[,"FORESTS_LABEL"])
+
+# formal tests ----
+analytics = create_analytics(container, results)
+summary(analytics)
+
+head(analytics@algorithm_summary)
+head(analytics@label_summary)
+head(analytics@document_summary)
+analytics@ensemble_summary # Ensemble Agreement
+
+# Cross Validation
+N=3
+cross_SVM = cross_validate(container,N,"SVM")
+cross_GLMNET = cross_validate(container,N,"GLMNET")
+cross_MAXENT = cross_validate(container,N,"MAXENT")
 
